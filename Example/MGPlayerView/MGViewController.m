1cfffe18b58c04b7de7b9f3409480809a17ba697
mal666666 <mal666666@163.com>
2020-08-19 14:50:11 +0800

    demo修改

--- Example/MGPlayerView/MGViewController.m
+++ Example/MGPlayerView/MGViewController.m
@@ -20,11 +20,11 @@
     [super viewDidLoad];
     PlayerView *playerView =[[PlayerView alloc]initWithFrame:CGRectMake(0, 80, self.view.bounds.size.width, self.view.bounds.size.width *9/16)];
     [self.view addSubview:playerView];
-    //[playerView setPlayUrl:@"http://stream1.shopch.jp/HLS/out1/prog_index.m3u8"];
-    [playerView setPlayUrl:@"http://154.8.159.252:7301/group1/M00/00/00/CqMY9F82TA2EKVz1AAAAAECmKyk899.mp4"];
+    [playerView setPlayUrl:@"http://stream1.shopch.jp/HLS/out1/prog_index.m3u8"];
+    //[playerView setPlayUrl:@"http://154.8.159.252:7301/group1/M00/00/00/CqMY9F82TA2EKVz1AAAAAECmKyk899.mp4"];
     [playerView supportFullScreenWithVC:self];
     playerView.delegateUI =self;
-    //[playerView play];
+    [playerView play];
 
 }
 
